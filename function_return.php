5fb90fc79e1f95d36883f5f40f248f8cf0ed9347
alaminwd <alamin.wd.bd@gmail.com>
2024-02-04 01:06:43 +0600

    initial commit

---
+++ function_return.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Function Return 
+
+// function avg($x, $y):int| float{         //function return type => int or float
+//     $avg = ($x+$y)/2;
+
+//     return $avg;
+// }
+
+// echo avg(7, 20);                             // echo avg(20, 20)+10;
+
+
+
+
+
+function avg($x, $y):void{   
+    $avg = ($x+$y)/2;
+
+}
+
+ avg(7, 20);               
